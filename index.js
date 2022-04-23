5ed8f20906f6a56c44e9237fc3db0627121f7494
Amber Frantz <amber.k.frantz@mgail.com>
2022-04-23 19:07:01 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,65 @@
 // Write your code here!
+
+const main = document.getElementById("main")
+main.remove()
+
+const newHeader = document.createElement("h1")
+newHeader.setAttribute("id", "victory")
+newHeader.textContent = ("Amber is the champion")
+document.body.append(newHeader);
+
+
+
+// const ul = document.getElementsByTagName("ul")[0];
+// const secondChild = ul.querySelector("li:nth-child(2)");
+// ul.removeChild(secondChild);
+
+
+
+// ul.remove();
+
+
+// const element = document.createElement("main")
+// document.body.append(element)
+
+
+// const ul = document.createElement("ul");
+
+// for (let i = 0; i < 3; i++) {
+//   const li = document.createElement("li");
+//   li.textContent = (i + 1).toString();
+//   ul.append(li);
+// }
+
+// element.append(ul);
+
+
+
+
+// const element = document.getElementById("main");
+// element.style.height = "300px";
+// element.style.backgroundColor = "#27647B";
+
+
+
+// element.textContent = "You've changed what's on the screen!";
+
+
+
+// element.style.fontSize = "24px";
+// element.style.marginLeft = "30px";
+// element.style.lineHeight = 2;
+
+
+
+// element.className = "pet-listing dog";
+
+
+
+// element.classList.remove("dog");
+// element.classList.add("cat", "sale");
+
+
+
+
+
